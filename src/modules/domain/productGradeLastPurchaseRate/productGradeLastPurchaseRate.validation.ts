6bcf8d0c50a59dada244dc4e1b477040bdaf5596
Pradeep <pradeep2ndsingh@gmail.com>
2026-06-19 13:53:15 +0530

    pagination

--- src/modules/domain/productGradeLastPurchaseRate/productGradeLastPurchaseRate.validation.ts
+++ src/modules/domain/productGradeLastPurchaseRate/productGradeLastPurchaseRate.validation.ts
@@ -5,8 +5,9 @@ import {
   statusFilterSchema,
 } from '../../common/common.validator.js';
 
-export const listProductGradeLastPurchaseRateQuerySchema =
-  paginationQuerySchema.merge(statusFilterSchema).extend({
+export const listProductGradeLastPurchaseRateQuerySchema = paginationQuerySchema
+  .merge(statusFilterSchema)
+  .extend({
     searchKey: z.string().optional(),
     productId: z.string().uuid().optional().or(z.literal('')),
     gradeId: z.string().uuid().optional().or(z.literal('')),
